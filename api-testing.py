772d999ff2639d00f42bf248c2fffd05c2776b9f
valeriomoroni95 <valeriomoroni95@gmail.com>
2019-11-19 15:49:07 +0100

    start work for all countries

--- api-testing.py
+++ api-testing.py
@@ -23,29 +23,46 @@ file = sc.textFile(hdis)
 country_hdi = sc.textFile(hdis).filter(filterNonExistant)
 country_hdi = country_hdi.map(extract2017Hdi)
 
-# for line in country_hdi.collect():
-# 	# words = re.split(',', line)
-# 	# country = words[1]
-# 	# hdi = words[-1]
-# 	country, hdi = line
-# 	print("{} : {}".format(country, hdi))
+# # for line in country_hdi.collect():
+# # 	# words = re.split(',', line)
+# # 	# country = words[1]
+# # 	# hdi = words[-1]
+# # 	country, hdi = line
+# # 	print("{} : {}".format(country, hdi))
 
-first = country_hdi.collect()[0]
-response = requests.get("http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country={}&api_key=101c6972f8adf89c5f3bdf67ff0efa0c&format=json".format(first[0]))
-response = json.loads(response.content)
+# first = country_hdi.collect()[0]
+# response = requests.get("http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country={}&api_key=101c6972f8adf89c5f3bdf67ff0efa0c&format=json".format(first[0]))
+# response = json.loads(response.content)
 
-#print(json.dumps(response, indent=2))
-#print(json.dumps(response['tracks']['track'][0]['mbid'], indent=2))
-mbid = response['tracks']['track'][0]['mbid'].replace('"', '')
+# #print(json.dumps(response, indent=2))
+# #print(json.dumps(response['tracks']['track'][0]['mbid'], indent=2))
+# mbid = response['tracks']['track'][0]['mbid'].replace('"', '')
 
-response = requests.get("http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=101c6972f8adf89c5f3bdf67ff0efa0c&mbid={}&format=json".format(mbid))
-response = json.loads(response.content)
+# response = requests.get("http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=101c6972f8adf89c5f3bdf67ff0efa0c&mbid={}&format=json".format(mbid))
+# response = json.loads(response.content)
 
-topTagsAndLinks = response['track']['toptags']['tag']
-sparkTopTagsAndLinks = sc.parallelize(topTagsAndLinks)
-sparkTopTags = sparkTopTagsAndLinks.map(lambda ob: ob['name'])
-print(sparkTopTags.collect())
+# topTagsAndLinks = response['track']['toptags']['tag']
+# sparkTopTagsAndLinks = sc.parallelize(topTagsAndLinks)
+# sparkTopTags = sparkTopTagsAndLinks.map(lambda ob: ob['name'])
+# print(sparkTopTags.collect())
 
-# for country, hdi in country_hdi.collect():	
-# 	response = requests.get("http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country={}&api_key=101c6972f8adf89c5f3bdf67ff0efa0c&format=json".format(country))
+# # for country, hdi in country_hdi.collect():	
+# # 	response = requests.get("http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country={}&api_key=101c6972f8adf89c5f3bdf67ff0efa0c&format=json".format(country))
+
+def tagsExtractor(track):
+	mbid = track['mbid'].replace('"', '')
+	response = requests.get("http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=101c6972f8adf89c5f3bdf67ff0efa0c&mbid={}&format=json".format(mbid))
+	response = json.loads(response.content)
+	topTagsAndLinks = response['track']['toptags']['tag']
+	sparkTopTagsAndLinks = sc.parallelize(topTagsAndLinks)
+	sparkTopTags = sparkTopTagsAndLinks.map(lambda ob: ob['name'])
+	return sparkTopTags.collect()
+
+for country, hdi in country_hdi.collect():
+	response = requests.get("http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country={}&api_key=101c6972f8adf89c5f3bdf67ff0efa0c&format=json".format(country))
+	response = json.loads(response.content)
+	sparkCountryTracks = sc.parallelize(response['tracks']['track'])
+	countryTags = sparkCountryTracks.map(tagsExtractor)
+	print(countryTags.collect())
+
 
