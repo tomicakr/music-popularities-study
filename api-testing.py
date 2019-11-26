e198ec6a5e5dc7b8ba6ab6618d4aa315f7aa4587
Valerio Moroni <Valerio@eduroam153162.eduroam.ucm.es>
2019-11-26 15:57:46 +0100

    tags for every country

--- api-testing.py
+++ api-testing.py
@@ -8,15 +8,15 @@ import json
 sc = SparkContext()
 
 def extract2017Hdi(line):
-	words = re.split(',', line)
-	country = words[1].replace('"', '').lower()
-	hdi = words[-1].replace('"', '')
-	return (country, float(hdi))
+    words = re.split(',', line)
+    country = words[1].replace('"', '').lower()
+    hdi = words[-1].replace('"', '')
+    return (country, float(hdi))
 
 def filterNonExistant(line):
-	words = re.split(',', line)
-	#print(words[-1])
-	return words[-1].replace('"', '').replace('.','').isdigit()
+    words = re.split(',', line)
+    #print(words[-1])
+    return words[-1].replace('"', '').replace('.','').isdigit()
 
 hdis = './hdis.csv'
 file = sc.textFile(hdis)
@@ -50,19 +50,36 @@ country_hdi = country_hdi.map(extract2017Hdi)
 # # 	response = requests.get("http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country={}&api_key=101c6972f8adf89c5f3bdf67ff0efa0c&format=json".format(country))
 
 def tagsExtractor(track):
-	mbid = track['mbid'].replace('"', '')
-	response = requests.get("http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=101c6972f8adf89c5f3bdf67ff0efa0c&mbid={}&format=json".format(mbid))
-	response = json.loads(response.content)
-	topTagsAndLinks = response['track']['toptags']['tag']
-	sparkTopTagsAndLinks = sc.parallelize(topTagsAndLinks)
-	sparkTopTags = sparkTopTagsAndLinks.map(lambda ob: ob['name'])
-	return sparkTopTags.collect()
+    mbid = track['mbid'].replace('"', '')
+    response = None
+    if mbid != '':
+        response = requests.get("http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=101c6972f8adf89c5f3bdf67ff0efa0c&mbid={}&format=json".format(mbid))
+    else:
+        artist = track['artist']['name']
+        trackName = track['name']
+        response = requests.get("http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=101c6972f8adf89c5f3bdf67ff0efa0c&artist={}&track={}&format=json".format(artist, trackName))
+    
+    response = json.loads(response.content)
+    # print(json.dumps(response, indent=2))
+    # print(response.keys())
+    if 'track' in response.keys():
+        topTagsAndLinks = response['track']['toptags']['tag']
+        #sparkTopTagsAndLinks = sc.parallelize(topTagsAndLinks)
+        tags = []
+        for tagsAndLinks in topTagsAndLinks:
+            tags.append(tagsAndLinks['name'])
+
+        #sparkTopTags = sparkTopTagsAndLinks.map(lambda ob: ob['name'])
+        return tags
 
 for country, hdi in country_hdi.collect():
-	response = requests.get("http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country={}&api_key=101c6972f8adf89c5f3bdf67ff0efa0c&format=json".format(country))
-	response = json.loads(response.content)
-	sparkCountryTracks = sc.parallelize(response['tracks']['track'])
-	countryTags = sparkCountryTracks.map(tagsExtractor)
-	print(countryTags.collect())
+    response = requests.get("http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country={}&api_key=101c6972f8adf89c5f3bdf67ff0efa0c&format=json&limit=500".format(country))
+    response = json.loads(response.content)
+    if 'tracks' in response.keys():
+        sparkCountryTracks = sc.parallelize(response['tracks']['track'])
+        countryTags = sparkCountryTracks.map(tagsExtractor)
+        print("{}: ".format(country))
+        for tags in countryTags.collect():
+            print("     {}".format(tags))
 
   
