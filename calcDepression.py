c5ea28c4d32f1e37ebbff3eac51134a8f25c761d
tomicakr <kravarscan.tomica@gmail.com>
2019-12-05 15:06:48 +0100

    test error messages

--- calcDepression.py
+++ calcDepression.py
@@ -12,12 +12,18 @@ printGroupsRanges(country_depression_groups)
 for group in country_depression_groups:
     depressionGroupsTags = []
     for country, hdi in group:
-        response = getSongs(country, 5)
-        response = json.loads(response.content)
-        if 'tracks' in response.keys():
-            sparkCountryTracks = sc.parallelize(response['tracks']['track'])
-            countryTags = sparkCountryTracks.map(tagsExtractor).flatMap(lambda x: x).map(lambda x: (x, 1)).groupByKey().map(lambda x: (x[0], sum(x[1])))
-            depressionGroupsTags.extend(list(countryTags.collect()))
+        i = 0
+        responseGot = True
+        while responseGot:
+            response = getSongs(country, 1000, 100000)
+            i += 1
+            response = json.loads(response.content)
+            print(response)
+            exit()
+            if 'tracks' in response.keys():
+                sparkCountryTracks = sc.parallelize(response['tracks']['track'])
+                countryTags = sparkCountryTracks.map(tagsExtractor).flatMap(lambda x: x).map(lambda x: (x, 1)).groupByKey().map(lambda x: (x[0], sum(x[1])))
+                depressionGroupsTags.extend(list(countryTags.collect()))
 
     depressionGroupsTags = sc.parallelize(depressionGroupsTags).groupByKey().map(lambda x: (x[0], sum(x[1])))
     summ =  depressionGroupsTags.map(lambda x: x[1]).reduce(lambda x, y: x + y)
