2a6729005d82869dac53a8437ec2c0c372841296
tomicakr <kravarscan.tomica@gmail.com>
2019-12-05 16:06:16 +0100

    print

--- calcDepression.py
+++ calcDepression.py
@@ -17,7 +17,7 @@ for group in country_depression_groups:
         i = 1
         while True:
             print(i)
-            response = getSongs(country, 100, i)
+            response = getSongs(country, 1000, i)
             i += 1
             #print(response.content)
             response = json.loads(response.content)
