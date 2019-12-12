da7bb802969e98ce3cd558df7791252c696da019
tomicakr <kravarscan.tomica@gmail.com>
2019-12-12 03:00:50 +0100

    refactor and add new images

--- hdi/analyzeHDI.py
+++ hdi/analyzeHDI.py
@@ -22,15 +22,12 @@ for i in range(1, n+1, 3):
         tags = []
         for line in groupFile:
             if firstLine:
-                print("Sad sam na file-u {}".format(i+j))
                 ranges = line.split('-')
                 rangesTotal.append(ranges)
                 firstLine = False
                 if j==0:
-                    print("0000000000000000000")
                     bigGroupRanges[0] = ranges[0]
                 elif j==2:
-                    print("2222222222222222222")
                     bigGroupRanges[1] = ranges[1]
                 continue
 
@@ -53,8 +50,8 @@ for i in range(1, n+1, 3):
     
     print("\n\n")
 
-    labels = [tag[0] for tag in sortedBigGroupPercetage[:10]]
-    counts = [tag[1] for tag in sortedBigGroupPercetage[:10]]
+    labels = [tag[0] for tag in sortedBigGroupPercetage[:15]]
+    counts = [tag[1] for tag in sortedBigGroupPercetage[:15]]
 
     for j in range(0,10):
         label, count = labels[j], counts[j]
@@ -66,39 +63,26 @@ for i in range(1, n+1, 3):
 
     groupFactor += 2
 
-    # x = np.arange(len(labels))  # the label locations
-    # width = 0.35  # the width of the bars
+    x = np.arange(len(labels))
+    width = 0.5
 
-    # fig, ax = plt.subplots()
-    # rects1 = ax.bar(x - width/2, counts, width)
+    fig, ax = plt.subplots()
+    rects1 = ax.bar(x, counts, width)
 
-    # ax.set_ylabel('Counts')
-    # ax.set_title('Tag counts')
-    # ax.set_xticks(x)
-    # ax.set_xticklabels(labels, rotation=90)
-    # ax.legend()
+    ax.set_ylabel('Percentages')
+    ax.set_title('The percentages of tags per genre')
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels, rotation=45)
 
-    # def autolabel(rects):
-    #     """Attach a text label above each bar in *rects*, displaying its height."""
-    #     for rect in rects:
-    #         height = rect.get_height()
-    #         ax.annotate('{}'.format(height),
-    #                     xy=(rect.get_x() + rect.get_width() / 2, height),
-    #                     xytext=(0, 3), 
-    #                     textcoords="offset points",
-    #                     ha='center', va='bottom')
+    fig.tight_layout()
+    plt.show()
 
-    # #autolabel(rects1)
-    # fig.tight_layout()
-    # plt.show()
+
 print(genresDict)
 genres = genresDict.keys()
-
 labels = genres
-
-x = np.arange(len(labels))  # the label locations
-width = 0.7  # the width of the bars
-
+x = np.arange(len(labels))
+width = 0.7
 fig, ax = plt.subplots()
 
 groupDictPerGenre = [[0 for _ in range(len(labels))] for i in range(5)]
@@ -113,36 +97,11 @@ for i in range(1,6):
     print(groupDictPerGenre[i-1])
     rects1 = ax.bar(x + (i-3)*(width/5), groupDictPerGenre[i-1], width/5, label="Group {}: range {}-{}".format(i, round(float(allBigGroupRanges[i-1][0]),3), round(float(allBigGroupRanges[i-1][1]),3)))
 
-# for key in genresDict.keys():
-#     a = 0
-#     for i in range(1,6):
-#         if i in genresDict[key]:
-#             groupDictPerGenre[i-1][a] = genresDict[key][i]
-#     a += 1
-
-# print(groupDictPerGenre)
-# a=0
-# print(x)
-# for i in range(5):
-#     rects1 = ax.bar((x-width/len(labels))*(a), groupDictPerGenre[i], width, label=key)
-#     a+=1
-
 ax.set_ylabel('Percent')
 ax.set_title('Percentage of tags in attribute groups')
 ax.set_xticks(x)
 ax.set_xticklabels(labels, rotation=45)
 ax.legend()
 
-def autolabel(rects):
-    """Attach a text label above each bar in *rects*, displaying its height."""
-    for rect in rects:
-        height = rect.get_height()
-        ax.annotate('{}'.format(height),
-                    xy=(rect.get_x() + rect.get_width() / 2, height),
-                    xytext=(0, 3), 
-                    textcoords="offset points",
-                    ha='center', va='bottom')
-
-#autolabel(rects1)
 fig.tight_layout()
 plt.show()
